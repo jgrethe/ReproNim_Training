a8d6a72b9a0e576808017bc3a04352ceb44bd7bd
jgrethe <Jeffrey.S.Grethe@alumni.usc.edu>
2017-11-11 12:33:58 -0500

    Updates for automatic run

--- repronim_challenge.R
+++ repronim_challenge.R
@@ -27,18 +27,18 @@ sink()
 # lm(formula = brainmask ~ sex, data = mydat)
 
 # Residuals:
-    # Min      1Q  Median      3Q     Max 
-# -142916  -37315    -870   43524  185699 
+    # Min      1Q  Median      3Q     Max
+# -142916  -37315    -870   43524  185699
 
 # Coefficients:
-             # Estimate Std. Error t value Pr(>|t|)    
+             # Estimate Std. Error t value Pr(>|t|)
 # (Intercept) 1.453e+06  2.004e+04   72.48   <2e-16 ***
-# sexM        8.635e-11  2.835e+04    0.00        1    
+# sexM        8.635e-11  2.835e+04    0.00        1
 # ---
 # Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
 # Residual standard error: 77630 on 28 degrees of freedom
-# Multiple R-squared:  8.032e-30,	Adjusted R-squared:  -0.03571 
+# Multiple R-squared:  8.032e-30,	Adjusted R-squared:  -0.03571
 # F-statistic: 2.249e-28 on 1 and 28 DF,  p-value: 1
 
 # ---------------------
@@ -47,9 +47,8 @@ sink()
 # ---------------------
 # A plot to go with the data
 mymeans <- with(mydat, aggregate(brainmask ~ sex, FUN = mean))
-pdf(file="plot.pdf", height=5, width=5)
+pdf(file="./plot/plot.pdf", height=5, width=5)
 boxplot(brainmask/1000 ~ sex, data = mydat, horizontal = TRUE, notch = TRUE, xlab = "brainmask volume (cm3)", ylab = "sex", col = "skyblue")
 with(mydat, points(x = mymeans[,2]/1000, y = c(1, 2), pch = 21, bg = "orange", cex = 3))
 dev.off()
-# ---------------------
-
+# --------------------- 
